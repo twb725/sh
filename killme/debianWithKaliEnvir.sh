d269c18b86be90d4b4d8b88b85d18e5c178f6241
w2eolbn <38269348+w2eolbn@users.noreply.github.com>
2018-04-11 11:06:29 +0800

    Create debianWithKaliEnvir.sh

---
+++ killme/debianWithKaliEnvir.sh
@@ -0,0 +1,13 @@
+#backup sources.list
+cp /etc/apt/sources.list /etc/apt/sources.list.back
+#add kali sources
+echo deb http://http.kali.org/kali kali-rolling main non-free contrib > /etc/apt/sources.list
+echo deb-src http://http.kali.org/kali kali-rolling main non-free contrib >> /etc/apt/sources.list
+echo # deb http://mirrors.ustc.edu.cn/kali kali-rolling main non-free contrib  >> /etc/apt/sources.list
+echo # deb-src http://mirrors.ustc.edu.cn/kali kali-rolling main non-free contrib  >> /etc/apt/sources.list
+#add apt-key 
+apt-key adv --keyserver keyserver.ubuntu.com --recv ED444FF07D8D0BF6
+#reflash local update packages & and upgrade it
+apt-get update 
+apt-get upgrade
+echo !!  it's all done   !!
