d49fba1997976eef97ed858e6b3098cbd6b30ba2
w2eolbn <38269348+w2eolbn@users.noreply.github.com>
2018-04-11 12:56:35 +0800

    Update debianWithKaliEnvir.sh

--- killme/debianWithKaliEnvir.sh
+++ killme/debianWithKaliEnvir.sh
@@ -12,6 +12,6 @@ apt-key adv --keyserver keyserver.ubuntu.com --recv ED444FF07D8D0BF6
 apt-get update 
 apt-get upgrade
 # install pip tool
-apt-get -y install python-pip tree screen
+apt-get install python-pip tree screen
 echo !!  it's all done   !!
 echo !!  it's all done   !!
