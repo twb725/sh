d6a218d56825d29b19f803f0af7fbc94531d316d
w2eolbn <38269348+w2eolbn@users.noreply.github.com>
2018-04-11 13:14:30 +0800

    Update install_ss.sh

--- killme/install_ss.sh
+++ killme/install_ss.sh
@@ -1,2 +1,9 @@
 #! /bin/bash
 pip install git+https://github.com/shadowsocks/shadowsocks.git@master
+wget https://github.com/w2eolbn/sh/raw/master/killme/shadowsocks.json
+echo ss was installed,you can run it before change PORT and PASSWORD in file shadowsocks.json.
+echo just run it by ..........
+echo .
+echo .
+echo ssserver -c /etc/shadowsocks.json -d start
+echo.
